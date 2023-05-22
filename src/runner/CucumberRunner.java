f163d4fd8d2f15dd2a18988bc91e840a91116109
alt-sidkishan <whistler.dummyemail@gmail.com>
2023-05-22 11:37:40 +0530

    jenkins

--- src/runner/CucumberRunner.java
+++ src/runner/CucumberRunner.java
@@ -1,11 +1,13 @@
 package runner;
 
 import org.junit.runner.RunWith;
+import org.testng.annotations.Test;
 
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 
+@Test
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "Features", glue = "stepDefinitions")
 public class CucumberRunner extends AbstractTestNGCucumberTests {
